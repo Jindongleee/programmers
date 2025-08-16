2f16d9c539d5f69f07a7f6c03bec4cbcc35d6c3e
Eddy <149242983+Jindongleee@users.noreply.github.com>
2025-08-17 01:09:34 +0900

    [level 2] Title: 올바른 괄호, Time: 16.62 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,14 +2,16 @@ import java.util.Stack;
 import java.util.*;
 
 class Solution {
+    
     boolean solution(String s) {
+        
         boolean answer = true;
+        
         Stack<Character> stack = new Stack<>();
         
-        if(s.length()==0)
-            return false;
         
         for(int i=0;i<s.length();i++){
+            
             char a = s.charAt(i);
             
             if(a==')'){
@@ -17,11 +19,14 @@ class Solution {
                     return false;
                 stack.pop();
             }
+            
             else 
                 stack.push(a);
+            
         }
         
         return stack.isEmpty();
         
     }
+    
 }
