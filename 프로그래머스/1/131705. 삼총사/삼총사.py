9df01fa7637ba81233401d376e0d386e5cf5f695
Eddy <149242983+Jindongleee@users.noreply.github.com>
2025-08-17 16:33:13 +0900

    [level 1] Title: 삼총사, Time: 0.11 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -8,8 +8,20 @@ def solution(number):
     
     number.index(min(number)) # 최소값을 갖는 인덱스 찾기
     
-    for i in combinations(number,3):
-        if sum(i) == 0:
-            answer+=1
+    # 1번 방법 -> combinations를 통해 3개의 부분집합 모두 찾기
+    # for i in combinations(number,3):
+    #     if sum(i) == 0:
+    #         answer+=1
+
+    # 2번 방법 3중 포인터
+    
+    if len(number) >= 0:
+        for i in range(len(number)-2):
+            for j in range(i+1,len(number)-1):
+                for k in range(j+1, len(number)):
+                    if (number[i]+number[j]+number[k]) == 0:
+                        answer+=1
+    else:
+        print("Don't enough length")
     
     return answer
