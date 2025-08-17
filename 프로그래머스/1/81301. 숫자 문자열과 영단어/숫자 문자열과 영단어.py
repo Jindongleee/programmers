5d869610916d05ee7bc5dbb7642d05f2cf92c079
Eddy <149242983+Jindongleee@users.noreply.github.com>
2025-08-17 18:24:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,79 +1,99 @@
 def solution(s):
     
     answer = []
-    
-    ask = []
-    
     # 한글자씩 조사 -> 아스키코드 값이 65 이상이다. 그러면 문자
     # 또는 isdigit 모두 숫자라면 -> True, 아니면 False
     str = []
     
-    for i in range(len(s)):
-        if s[i].isdigit():
-            if len(str) != 0:
-                if ''.join(str) == "zero":
-                    answer.append("0")
-                    str = []
-                elif ''.join(str) == "one":
-                    answer.append("1")
-                    str = []
-                elif ''.join(str) == "two":
-                    answer.append("2")
-                    str = []
-                elif ''.join(str) == "three":
-                    answer.append("3")
-                    str = []
-                elif ''.join(str) == "four":
-                    answer.append("4")
-                    str = []
-                elif ''.join(str) == "five":
-                    answer.append("5")
-                    str = []
-                elif ''.join(str) == "six":
-                    answer.append("6")
-                    str = []
-                elif ''.join(str) == "seven":
-                    answer.append("7")
-                    str = []
-                elif ''.join(str) == "eight":
-                    answer.append("8")
-                    str = []
-                elif ''.join(str) == "nine":
-                    answer.append("9")
-                    str = []
-            answer.append(s[i]) # 숫자 담기
-        else:
-            str.append(s[i]) 
-            if ''.join(str) == "zero":
-                answer.append("0")
-                str = []
-            elif ''.join(str) == "one":
-                answer.append("1")
-                str = []
-            elif ''.join(str) == "two":
-                answer.append("2")
-                str = []
-            elif ''.join(str) == "three":
-                answer.append("3")
-                str = []
-            elif ''.join(str) == "four":
-                answer.append("4")
-                str = []
-            elif ''.join(str) == "five":
-                answer.append("5")
-                str = []
-            elif ''.join(str) == "six":
-                answer.append("6")
-                str = []
-            elif ''.join(str) == "seven":
-                answer.append("7")
-                str = []
-            elif ''.join(str) == "eight":
-                answer.append("8")
-                str = []
-            elif ''.join(str) == "nine":
-                answer.append("9")
-                str = []
+    # 1. 노가다 방식
+    # for i in range(len(s)):
+    #     if s[i].isdigit():
+    #         if len(str) != 0:
+    #             if ''.join(str) == "zero":
+    #                 answer.append("0")
+    #                 str = []
+    #             elif ''.join(str) == "one":
+    #                 answer.append("1")
+    #                 str = []
+    #             elif ''.join(str) == "two":
+    #                 answer.append("2")
+    #                 str = []
+    #             elif ''.join(str) == "three":
+    #                 answer.append("3")
+    #                 str = []
+    #             elif ''.join(str) == "four":
+    #                 answer.append("4")
+    #                 str = []
+    #             elif ''.join(str) == "five":
+    #                 answer.append("5")
+    #                 str = []
+    #             elif ''.join(str) == "six":
+    #                 answer.append("6")
+    #                 str = []
+    #             elif ''.join(str) == "seven":
+    #                 answer.append("7")
+    #                 str = []
+    #             elif ''.join(str) == "eight":
+    #                 answer.append("8")
+    #                 str = []
+    #             elif ''.join(str) == "nine":
+    #                 answer.append("9")
+    #                 str = []
+    #         answer.append(s[i]) # 숫자 담기
+    #     else:
+    #         str.append(s[i]) 
+    #         if ''.join(str) == "zero":
+    #             answer.append("0")
+    #             str = []
+    #         elif ''.join(str) == "one":
+    #             answer.append("1")
+    #             str = []
+    #         elif ''.join(str) == "two":
+    #             answer.append("2")
+    #             str = []
+    #         elif ''.join(str) == "three":
+    #             answer.append("3")
+    #             str = []
+    #         elif ''.join(str) == "four":
+    #             answer.append("4")
+    #             str = []
+    #         elif ''.join(str) == "five":
+    #             answer.append("5")
+    #             str = []
+    #         elif ''.join(str) == "six":
+    #             answer.append("6")
+    #             str = []
+    #         elif ''.join(str) == "seven":
+    #             answer.append("7")
+    #             str = []
+    #         elif ''.join(str) == "eight":
+    #             answer.append("8")
+    #             str = []
+    #         elif ''.join(str) == "nine":
+    #             answer.append("9")
+    #             str = []
+    #   return int(''.join(answer))
             
-    
-    return int(''.join(answer))
+    # 2. 분기 방식
+    if 'one' in s:
+        s= s.replace('one', '1')
+    if 'zero' in s:
+        s= s.replace('zero', '0')
+    if 'two' in s:
+        s= s.replace('two', '2')
+    if 'three' in s:
+        s= s.replace('three', '3')
+    if 'four' in s:
+        s= s.replace('four', '4')
+    if 'five' in s:
+        s= s.replace('five', '5')
+    if 'six' in s:
+        s= s.replace('six', '6')
+    if 'seven' in s:
+        s= s.replace('seven', '7')
+    if 'eight' in s:
+        s= s.replace('eight', '8')
+    if 'nine' in s:
+        s= s.replace('nine', '9')
+
+    return int(s)
